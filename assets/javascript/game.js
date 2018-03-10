142f5ab20a7744aad6cd4f66e6e383ecf42fa7ff
Rounder1 <33045010+Rounder1@users.noreply.github.com>
2018-03-09 22:20:37 -0500

    Add files via upload

---
+++ assets/javascript/game.js
@@ -0,0 +1,61 @@
+
+$(document).ready(function(){
+
+    var playerWins = 0;
+    var playerLosses = 0;    
+    var goalNumber = Math.floor(Math.random() * (120 - 18 +1)) + 18;
+    var currentNumber = 0;
+    var crystalRed = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    var crystalBlue = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    var crystalYellow = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    var crystalGreen = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+
+
+    $("#player_wins").text(playerWins);
+    $("#player_losses").text(playerLosses);
+    $("#goal_number").text(goalNumber);
+    $("#current_number").text(currentNumber);
+
+    function newGame () {
+        goalNumber = Math.floor(Math.random() * (120 - 18 +1)) + 18;
+        currentNumber = 0;
+        crystalRed = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        crystalBlue = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        crystalYellow = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        crystalGreen = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+
+        $("#player_wins").text(playerWins);
+        $("#player_losses").text(playerLosses);
+        $("#goal_number").text(goalNumber);
+        $("#current_number").text(currentNumber);
+    }
+
+
+    $(".crystal_button").click(function(){
+        var crystalClicked = event.target.id; // grabs the ID of the element that triggered the event 
+
+        $("#win_loss_alert").text("");
+            
+        if (crystalClicked === "red_crystal") {
+            currentNumber += crystalRed;
+        } else if (crystalClicked === "blue_crystal") {
+            currentNumber += crystalBlue;
+        } else if (crystalClicked === "yellow_crystal") {
+            currentNumber += crystalYellow;
+        } else if (crystalClicked === "green_crystal") {
+            currentNumber += crystalGreen;
+        }
+
+        $("#current_number").text(currentNumber);
+
+        if (currentNumber === goalNumber) {
+            $("#win_loss_alert").text("You Win!");
+            playerWins++;
+            newGame();
+        } else if (currentNumber > goalNumber) {
+            $("#win_loss_alert").text("You Loss");
+            playerLosses++;
+            newGame();
+        }
+    });
+ });
